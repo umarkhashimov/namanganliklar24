4ea8531197979f02de3d3caee31fa2190926536c
Umar Hoshimov <umarkhashimov05@gmail.com>
2025-01-13 19:12:24 +0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import './index.css'
+import App from './App.jsx'
+
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.js'
+
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App/>}></Route>
+    </Routes>
+  </BrowserRouter>
+)
