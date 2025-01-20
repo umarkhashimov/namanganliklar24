baf7f9faee623d303aad09f6246eef8bdd7cb314
Umar Hoshimov <umarkhashimov05@gmail.com>
2025-01-20 18:39:41 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,66 @@
 import Navbar from "./components/Navbar.jsx"
 
+import virusIcon from './assets/virus@2x.png'
+import calendarIcon from './assets/ic_outline-date-range.png'
 function App() {
 
   return (
     <>
       <Navbar />
+
+      <div className="container mt-4">
+        <div className="row subheading px-4">
+          <div className="col-3 py-4 d-flex justify-content-center">
+            <div className="d-flex align-items-center gap-3 h-100">
+              <img src={virusIcon} alt="" className="img-fluid" />
+              <p className="m-0">Коронавирус COVID-19 в Узбекистане</p>
+            </div>
+          </div>
+          <div className="col-3 py-4 d-flex justify-content-center">
+            <div className="d-flex align-items-center gap-3 h-100">
+              <img src={virusIcon} alt="" className="img-fluid" />
+              <p className="m-0">Коронавирус COVID-19 в Узбекистане</p>
+            </div>
+          </div>
+          <div className="col-3 py-4 d-flex justify-content-center">
+            <div className="d-flex align-items-center gap-3 h-100">
+              <img src={virusIcon} alt="" className="img-fluid" />
+              <p className="m-0">Коронавирус COVID-19 в Узбекистане</p>
+            </div>
+          </div>
+          <div className="col-3 py-4 d-flex justify-content-center">
+            <div className="d-flex align-items-center gap-3 h-100">
+              <img src={virusIcon} alt="" className="img-fluid" />
+              <p className="m-0">Коронавирус COVID-19 в Узбекистане</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="container">
+        <h4>Политика</h4>
+        <div className="row">
+          <div className="col-8">
+
+            <div className="row news-card my-3">
+              <div className="col-4">
+                <img className="img-fluid" src="https://s3-alpha-sig.figma.com/img/e910/f547/856ddde0d5f64f965c08fc302bba2a48?Expires=1737936000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ce1mus~JN-pkTqkM8OsMZBLw-vqDVbu3KGeco9N-g~RS0aQnvVtRpEyfe4AnyE8u3hsaJ4FU-Vxy~mYioQEnJVdXI9QOTYW0~hktj6ik9kThodqaKKuYvYEYBsiinKVTOPhUs8qF9EAnnPwDcxzUhFpeXMHUnVyTx5FdU3PSvjhbi~8Pre0bUyRbOZUpGCvKuMM4aAXaJtz6P62fQEM8DgQ5yAgT8617Ssz4IJFfTiWskSFktxwnQyX57Nsp6GUdbEObDX2zBQ2swgCkYnCdp56PtOObBAX1xlms8PciFT-7-cG5F1vD6DytiaaI4e4-7RVhzokDsvc4pDfGqDht2g__" alt="" />
+              </div>
+              <div className="col-8 d-flex flex-column justify-content-between">
+                <p className="title">По факту прорыва Сардобинского водохранилища возбуждено уголовное дело</p>
+                <p className="content">После прорыва дамбы Сардобинского водохранилища возбуждено уголовное дело, сообщили в пресс-службе Генпрокуратуры Узбекистана.</p>
+                <p className="date m-0"><img src={calendarIcon} alt="" />11:31 / 15.05.2020</p>
+              </div>
+            </div>
+            
+
+          </div>
+          <div className="col-4">
+
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,49 @@
 
 import { Link } from "react-router"
-
-function Navbar(){
+import telegramLogo from '../assets/cib_telegram.png'
+import searchIcon from '../assets/mdi_magnify.png'
+import RussianFlag from '../assets/cif_ru.png'
+import navbarBanner from '../assets/Rectangle 3.jpg'
+function Navbar() {
 
     return (
         <>
-        <div className="my-navbar pt-4 pb-4">
-            <div className="container">
-                <div>
+            <div className="my-navbar pt-4 pb-4">
+                <div className="container">
+                    <div className="d-flex justify-content-between align-items-center">
+                        <div className="d-flex align-items-center gap-3">
+                            <Link to='/' className="navbar-logo">NAMANGANLIKLAR24</Link>
+                            <div className="d-flex gap-3 currencies">
+                                <p className="d-inline"><span>$</span> 1000.2</p>
+                                <p className="d-inline"><span>P</span> 138.26</p>
+                                <p className="d-inline"><span>E</span> 10988.72</p>
+                            </div>
+                        </div>
+                        <div className="d-flex gap-3">
+                            <button type="button" className="navbar-btn"><img src={searchIcon} alt="" /></button>
+                            <button type="button" className="navbar-btn d-flex align-items-center gap-1"><img src={RussianFlag} alt="" />РУ</button>
+                            <Link to='https://t.me/fayz_umar_g1' type="button" className="navbar-telegram"><img src={telegramLogo} className="me-2" alt="" />Подписатся</Link>
+                        </div>
+                    </div>
+                    <div className="bg-white">
+                        <ul className="list-unstyled d-flex justify-content-evenly navbar-links py-3 mt-4">
+                            <li><Link>Узбекистана</Link></li>
+                            <li><Link>Мир</Link></li>
+                            <li><Link>Экономика</Link></li>
+                            <li><Link className="active">Политика</Link></li>
+                            <li><Link>Общество</Link></li>
+                            <li><Link>Технологии</Link></li>
+                            <li><Link>Спорт</Link></li>
+                            <li><Link>Культура</Link></li>
+                            <li><Link>Проишествия</Link></li>
+                            <li><Link>Туризм</Link></li>
+                        </ul>
+                    </div>
                     <div>
-                        <Link to='/' className="navbar-logo">NAMANGANLIKLAR24</Link>
+                        <img src={navbarBanner} className="img-fluid w-100 navbar-banner" alt="" />
                     </div>
                 </div>
             </div>
-        </div>
         </>
     )
 }
